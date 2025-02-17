a4b6c369e4d03ba22701a718edc16f0928f81d5a
Emmanuel Valverde Ramos <evalverde@voxelgroup.net>
2025-02-17 11:09:24 +0100

    chore: create new test for the class Report that will encapsulate the logic on the report_checker

---
+++ test_report.py
@@ -0,0 +1,14 @@
+class Report:
+    def __init__(self, report_data):
+        self.report_data = report_data
+
+    def is_safe(self) -> bool:
+        pass
+
+
+class TestReport:
+    def test_given_a_report_where_levels_decrease_on_one_or_two_per_level_then_the_report_is_secure(self):
+        report = Report([7, 6, 4, 2, 1])
+
+        assert True == report.is_safe()
+
