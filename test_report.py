211e96fcfaacfeb8746b66ff62c93db0b623bed3
Emmanuel Valverde Ramos <evalverde@voxelgroup.net>
2025-02-17 15:07:42 +0100

    chore: test_given_an_decreasing_report_when_levels_decrease_over_four_levels_then_the_report_is_unsecure

--- test_report.py
+++ test_report.py
@@ -26,6 +26,12 @@ class Report:
             return True
 
 
+# ✅ 7 6 4 2 1
+# ✅ 1 2 7 8 9
+# ✅ 9 7 6 2 1
+# ✅ 1 3 2 4 5
+# 8 6 4 4 1
+# 1 3 6 7 9
 class TestReport:
     def test_given_a_report_when_levels_decrease_on_one_or_two_per_level_then_the_report_is_secure(self):
         report = Report([7, 6, 4, 2, 1])
@@ -39,5 +45,10 @@ class TestReport:
 
     def test_given_an_increasing_report_when_levels_increase_over_five_levels_then_the_report_is_unsecure(self):
         report = Report([1, 2, 7, 8, 9])
+
+        assert False == report.is_safe()
+
+    def test_given_an_decreasing_report_when_levels_decrease_over_four_levels_then_the_report_is_unsecure(self):
+        report = Report([9, 7, 6, 2, 1])
 
         assert False == report.is_safe()
