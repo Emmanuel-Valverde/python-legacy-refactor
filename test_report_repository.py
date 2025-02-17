ca6146058eee963c5ddc719dcd6d72978e41139e
Emmanuel Valverde Ramos <evalverde@voxelgroup.net>
2025-02-17 18:01:11 +0100

    chore: solving an issue on the test case

--- test_report_repository.py
+++ test_report_repository.py
@@ -21,7 +21,7 @@ class TestReportRepository:
         expected_reports = [
             Report([7, 6, 4, 2, 1]),
             Report([1, 2, 7, 8, 9]),
-            Report([7, 6, 4, 2, 1]),
+            Report([9, 7, 6, 2, 1]),
             Report([1, 3, 2, 4, 5]),
             Report([8, 6, 4, 4, 1]),
             Report([1, 3, 6, 7, 9])
