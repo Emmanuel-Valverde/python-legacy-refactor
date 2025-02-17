5bfccccf5a2a36a2bc117cdd0142d9a3572ea3b0
Emmanuel Valverde Ramos <evalverde@voxelgroup.net>
2025-02-17 09:54:22 +0100

    chore: extract function to be able to work with awkward dependencies

--- report_checker.py
+++ report_checker.py
@@ -1,23 +1,30 @@
 f = open("input.txt", "r")
-count = 0
-for i in f:
-	j=list(map(int, i.split()))
 
-	if j[0] > j[1]:
-		m = -1
-	elif j[0] < j [1]:
-		m = 1
-	else:
-		continue
 
-	for x in range(len(j) -1):
-		if 0 < (j[x + 1] -j [x] ) * m < 4:
-			t = True
+def check_report():
+	count = 0
+	for i in f:
+		j = list(map(int, i.split()))
+
+		if j[0] > j[1]:
+			m = -1
+		elif j[0] < j[1]:
+			m = 1
 		else:
-			t = False
-			break
+			continue
 
-	if t:
-		count += 1
+		for x in range(len(j) - 1):
+			if 0 < (j[x + 1] - j[x]) * m < 4:
+				t = True
+			else:
+				t = False
+				break
 
-print(count)
+		if t:
+			count += 1
+	return count
+
+
+
+
+print(check_report())
