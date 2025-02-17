3a7d2dda613482f63f32c2164d9414fdbf05aae9
Emmanuel Valverde Ramos <evalverde@voxelgroup.net>
2025-02-17 18:15:06 +0100

    chore: simplify the code to be more readable

--- report_checker.py
+++ report_checker.py
@@ -2,12 +2,10 @@ from test_report_repository import ReportRepository
 
 
 def check_report(report_repository: ReportRepository):
-	count = 0
 	reports = report_repository.get_all()
-	for report in reports:
-		if report.is_safe():
-			count += 1
-	return count
+	safe_reports = [report for report in reports if report.is_safe()]
+	return len(safe_reports)
+
 
 if __name__ == "__main__":
 	print(check_report(ReportRepository("input.txt")))
