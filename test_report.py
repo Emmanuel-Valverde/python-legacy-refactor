3fa58914a352269d884a48f6a9959b8482d96fc2
Emmanuel Valverde Ramos <evalverde@voxelgroup.net>
2025-02-17 13:08:39 +0100

    test: create a new test that will cover the early return

--- test_report.py
+++ test_report.py
@@ -7,8 +7,12 @@ class Report:
 
 
 class TestReport:
-    def test_given_a_report_where_levels_decrease_on_one_or_two_per_level_then_the_report_is_secure(self):
+    def test_given_a_report_when_levels_decrease_on_one_or_two_per_level_then_the_report_is_secure(self):
         report = Report([7, 6, 4, 2, 1])
 
         assert True == report.is_safe()
 
+    def test_given_a_report_when_levels_increase_and_decrease_then_the_report_is_unsecure(self):
+        report = Report([1, 3, 2, 4, 5])
+
+        assert False == report.is_safe()
